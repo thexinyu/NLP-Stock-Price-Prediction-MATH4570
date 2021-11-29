b95fd21c581e32d2e2dcd3bdbaf62bc9daea1ea6
Marco tortolani <tortolani.mar@northeastern.edu>
2021-11-28 22:31:23 -0500

    changes to stock files

---
+++ PerformanceTesting.py
@@ -0,0 +1,70 @@
+from os import error
+from PdfCleaner import PdfCleaner
+from StockPuller import StockPuller
+   
+class PerformanceTester():
+    def __init__(self) -> None:
+        articleData = []
+       
+        timeFrame = 'day'
+        preferredTimespan = 1
+   
+    def loadArticles(self, articleList):
+        '''
+        Loads article data [ticker, date, transcript] into the PerformanceTester interior variable
+ 
+        Parameters:
+            articleList (list): a nested list in the form [ticker, date, transcript].This is stock ticker, the date of a report, and the transcript text
+        '''
+        self.articleData = articleList
+   
+    def setTimeframe(self, frame='day', span=1):
+        '''
+        Sets timeframe
+ 
+        Parameters:
+            frame (str):
+            span (int):
+        '''
+        if frame == 'hour':
+            self.timeframe = 'hour'
+        elif frame == 'day':
+            self.timeframe = 'day'
+        else:
+            raise Exception('invalid frame')
+        self.preferredTimespan = span
+   
+    def aquireTargetValues(self):
+        '''
+        Creates x values (list of articles) and y values (list of stock price change after article release) for nlp training
+        '''
+        x_values = [article[2] for article in self.articleData]
+        y_values = []
+        Puller = StockPuller()
+        for article in self.articleData:
+            if self.timeframe == 'day':
+                priceChange = Puller.changeOverDays(article[0], article[1], self.preferredTimespan)
+            elif self.timeframe == 'hour':
+                priceChange = Puller.changeOverHours(article[0], article[1], self.preferredTimespan)
+            else:
+                raise Exception('Please choose a reference type (\'hour\' or \'day\') using function setTimeframe')
+            y_values.append(priceChange)
+        return [x_values, y_values]
+       
+           
+               
+               
+ 
+# Test
+ 
+# Get clean stock transcripts
+origin = PdfCleaner("AAPL Transcripts/APPL_2016Q3.pdf")
+cleaned_text = origin.clean_stopwords_punctuation()
+ 
+Tester = PerformanceTester()
+articles = [['AAPL', '2019-10-02', cleaned_text], ['AAPL', '2019-10-02', cleaned_text]]
+Tester.loadArticles(articles)
+Tester.setTimeframe('day', 2)
+xy = Tester.aquireTargetValues()
+print(xy[0], '\n\n\n', xy[1])
+
--- StockPuller.py
+++ StockPuller.py
@@ -3,30 +3,30 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime
 from datetime import timedelta
-
+ 
 class StockPuller:
     def __init__(self):
-        self. tickerList = ('AAPL', 
-                            'MSFT', 
-                            'AMZN', 
+        self. tickerList = ('AAPL',
+                            'MSFT',
+                            'AMZN',
                             'FB',
-                            'GOOG', 
-                            'GOOGL', 
-                            'TSLA', 
-                            'NVDA', 
-                            'PYPL', 
-                            'ASML', 
-                            'INTC', 
-                            'CMCSA', 
-                            'NFLX', 
-                            'ADBE', 
-                            'CSCO', 
-                            'PEP', 
-                            'AVGO', 
-                            'TXN', 
-                            'PDD', 
+                            'GOOG',
+                            'GOOGL',
+                            'TSLA',
+                            'NVDA',
+                            'PYPL',
+                            'ASML',
+                            'INTC',
+                            'CMCSA',
+                            'NFLX',
+                            'ADBE',
+                            'CSCO',
+                            'PEP',
+                            'AVGO',
+                            'TXN',
+                            'PDD',
                             'TMUS')
-        
+       
     # get historical market data
     def plotAll(self):
         for ticker in self.tickerList:
@@ -35,66 +35,78 @@ class StockPuller:
             hist["Close"].plot(figsize=(16,9))
         plt.legend(self.tickerList)
         plt.show()
-    
+   
     def plotStock(self, ticker):
         stock = yf.Ticker(ticker)
         hist = stock.history(period='5y', interval='1d')
         hist["Close"].plot(figsize=(16,9))
         plt.legend([ticker])
         plt.show()
-
+ 
     def dailyData(self, ticker):
         stock = yf.Ticker(ticker)
         stockData = stock.history(period='5y',interval='1d')
         print(stockData)
-
+ 
     def changeOverDays(self, ticker, startDate, days):
         '''
         Returns the % change of a stocks value in a certan timeframe
-
+ 
         Parameters:
         ticker (str): stock ticker (ex. 'AAPL')
         startDate (str): the day which to start the comparison (ex. '2021-11-02')
         days (int): days that pass on which to compare closing values
+       
+        Bugfix:
+        market isnt open on weekends
         '''
         stock = yf.Ticker(ticker)
         stockData = stock.history(period='5y', interval='1d')
-
+ 
         startDatetime = datetime.strptime(startDate, '%Y-%m-%d')
         endDatetime = startDatetime + timedelta(days=days)
-
+ 
         startPrice = stockData.loc[startDatetime]['Open']
         endPrice = stockData.loc[endDatetime]['Close']
         changeInPrice = startPrice / endPrice
         return changeInPrice - 1
-
+ 
     def changeOverHours(self, ticker, startDate, hours):
         '''
         Returns the % change of a stocks value in a certan timeframe
-
+ 
         Parameters:
         ticker (str): stock ticker (ex. 'AAPL')
-        startDate (str): the day which to start the comparison (ex. '2021-11-02')
+        startDate (str): the day and time which to start the comparison (ex. '2021-11-02 11:30:00') NOTE: only put in 30 minute intervals
         hours (int): hours that pass on which to compare closing values
+       
+        Bugfix:
+        market isnt open on weekends
+        only takes in half hour inputs
         '''
         stock = yf.Ticker(ticker)
         stockData = stock.history(period='2y', interval='1h')
         stockData.index = stockData.index.tz_localize(None)
-        
+       
         # TO DO: limit to only allow times within market open/closing hours (9:30am-4:30pm)
-        startDatetime = datetime.strptime(startDate, '%Y-%m-%d %H:%M:%S')
-        endDatetime = startDatetime + timedelta(hours=hours)
-
+        try:
+            startDatetime = datetime.strptime(startDate, '%Y-%m-%d %H:%M:%S')
+            endDatetime = startDatetime + timedelta(hours=hours)
+        except:
+            raise Exception('You are checking for time after market closing hours')
+ 
         startPrice = stockData.loc[startDatetime]['Open']
         endPrice = stockData.loc[endDatetime]['Close']
         changeInPrice = (startPrice / endPrice) - 1
         return changeInPrice
-        
+   
+ 
+ 
+ 
+# Puller = StockPuller()  
+# Puller.plotAll()
+# Puller.plotStock('AAPL')
+# Puller.dailyData('AAPL')
+# print(Puller.changeOverDays('AAPL', '2019-10-02', 50))
+# print(Puller.changeOverHours('AAPL', '2019-11-11 11:30:00', 25))
 
-  
-SP = StockPuller()  
-SP.plotAll()
-SP.plotStock('AAPL')
-SP.dailyData('AAPL')
-print(SP.changeOverDays('AAPL', '2019-10-02', 50))
-print(SP.changeOverHours('AAPL', '2019-11-11 11:30:00', 25))
