8d4a9e34971c855f833fd3cff18484971c8dfa3b
Xinyu Wu <xinyuwu421@gmail.com>
2021-12-08 15:44:20 -0500

    Update PdfCleaner.py

--- PdfCleaner.py
+++ PdfCleaner.py
@@ -6,6 +6,7 @@ import re
 class PdfCleaner:
 
   def __init__ (self,pdf_file_path):
+   """ Use provided path to get and read pdf file """
     self.__path__ = pdf_file_path
     text = ""
     with pdfplumber.open(self.__path__) as pdf:
@@ -17,18 +18,23 @@ class PdfCleaner:
     self.__nonum_text__ = self.clean_nums()
 
   def lenBeforeClean(self):
+    """ Returns length of pdf file text before cleaning """
     return len(self.__original_text__)
 
   def lenAfterClean(self):
+    """ Returns length of pdf file text after cleaning """
     return len(self.__cleaned_text__)
 
   def print_originalText(self):
+    """ Prints out original text before cleaning"""
     print(self.__original_text__)
 
   def print_cleanedText(self):
+    """ Prints out cleaned tet """
     print(self.__cleaned_text__)
 
   def clean_stopwords_punctuation(self):
+    """ Removes stopwords from text"""
     cleaned_text = remove_stopwords(self.__original_text__)
     punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~©'''
     for ele in cleaned_text:
@@ -37,13 +43,6 @@ class PdfCleaner:
     return cleaned_text
 
   def clean_nums(self):
+    """ Removes numbers from text """
     nonum_text = re.sub(r'\d+', '', self.__cleaned_text__)
     return nonum_text
-
-
-# Test
-# origin = PdfCleaner("APPL_2016Q3.pdf")
-# cleaned_text=origin.clean_stopwords_punctuation()
-# origin.print_cleanedText()
-# print(origin.lenBeforeClean())
-# print(origin.lenAfterClean())
