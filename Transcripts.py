539001d01a1f8e6f6ee1849bcc900bbda619ba7f
Xinyu Wu <xinyuwu421@gmail.com>
2021-12-08 11:31:10 -0500

    Update Transcripts.py

--- Transcripts.py
+++ Transcripts.py
@@ -46,19 +46,19 @@ class Transcripts:
                 lst_cleaned.append(dct_cleaned)
         return lst_cleaned
 
-class Database:
-    def __init__(self):
-        client = MongoClient()
-        client.drop_database('transcripts')
-        self.db = client.transcripts
+# class Database:
+#     def __init__(self):
+#         client = MongoClient()
+#         client.drop_database('transcripts')
+#         self.db = client.transcripts
 
-    def store_data(self, tickers_lst):
-        for t in tickers_lst:
-            store = Transcripts(t)
-            transcript = store.create_dct()
-            self.db.transcript.insert_many(transcript)
-            print(t + " transcripts stored successfully")
-        return self.db
+#     def store_data(self, tickers_lst):
+#         for t in tickers_lst:
+#             store = Transcripts(t)
+#             transcript = store.create_dct()
+#             self.db.transcript.insert_many(transcript)
+#             print(t + " transcripts stored successfully")
+#         return self.db
 
 def main():
     tickers = ['AAPL']
