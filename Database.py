5a791646e255381cd6c569dfaefe161c23a87e70
Xinyu Wu <xinyuwu421@gmail.com>
2021-12-08 11:32:32 -0500

    Update Database.py

--- Database.py
+++ Database.py
@@ -1,4 +1,14 @@
 from Transcripts import Transcripts
+from PdfCleaner import PdfCleaner
+from os import listdir
+from os.path import isfile, join
+from PerformanceTester import PerformanceTester
+import pdfplumber
+import json
+import pandas as pd
+import numpy as np
+import requests
+from pymongo import MongoClient
 
 class Database:
     def __init__(self):
